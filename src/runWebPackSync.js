159f31f660c707ffe93c37f414a344665cc128e6
cybice <istarkov@gmail.com>
2016-01-02 06:24:34 +0300

    Fix comment

--- src/runWebPackSync.js
+++ src/runWebPackSync.js
@@ -10,7 +10,7 @@ export default ({ path, configPath, config, verbose }) => {
   const rnd = `${(new Date()).getTime()}_${Math.round(1000000 * Math.random())}`;
   const outPath = join(config.output.path || DEFAULT_OUTPUT_PATH, `.webpack.res.${rnd}.js`);
 
-  // I need to run webpack via execFileSync because I have not find the way how to run
+  // I need to run webpack via execFileSync because I have not found the way how to run
   // babel visitors asynchronously or run webpack compile synchronously
   const webPackStdOut = require('child_process')
     .execFileSync(webPackPath, [path, outPath, '--config', configPath, '--colors', '--bail']);
