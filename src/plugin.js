d733ee234d04dd8dc009c818c6b4c9163be5c4bb
kaleidoc0de <martindenyer@outlook.com>
2017-02-28 22:23:45 +0000

    add webpack 2 config support (#263)
    target correct property for the version of webpack being used

--- src/plugin.js
+++ src/plugin.js
@@ -255,7 +255,8 @@ More information at issue #36`
           return;
         }
 
-        if (config.module.loaders.some((l) => l.test.test(filePath) || l.test.test(fileAbsPath))) {
+        const rules = ('rules' in config.module) ? 'rules' : 'loaders';
+        if (config.module[rules].some((l) => l.test.test(filePath) || l.test.test(fileAbsPath))) {
           if (isJSFile(fileAbsPath) && skipJs) {
             // js and jsx files in loaders is unsupported by webpack-loader plugin.
             // all babel settings in loader will be skipped`
